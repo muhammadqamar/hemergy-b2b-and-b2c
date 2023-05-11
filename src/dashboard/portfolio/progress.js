a66c64f709e590b8e7cae7565faec87a75af6b81
muhammadqamar <muhammadqamar111@gmail.com>
2023-05-11 14:57:07 +0500

    add balance

--- src/dashboard/portfolio/progress.js
+++ src/dashboard/portfolio/progress.js
@@ -1,21 +1,31 @@
-import Image from "next/image";
-import { CircularProgressbarWithChildren, buildStyles } from "react-circular-progressbar";
-import "react-circular-progressbar/dist/styles.css";
+import Image from 'next/image';
+import {
+  CircularProgressbarWithChildren,
+  buildStyles,
+} from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
-const Progress = () => {
+const Progress = ({ balance }) => {
   return (
     <div className="wallet-progress">
       <div className="w-full h-full">
         <CircularProgressbarWithChildren
           styles={buildStyles({
-            pathColor: "#92a0ff",
-            trailColor: "#6b7efd",
+            pathColor: '#92a0ff',
+            trailColor: '#6b7efd',
           })}
-          value={40}
+          value={10}
         >
           <div className="progress-about">
-            <Image src="/images/payments.svg" alt="USDC" width={32} height={32} />
-            <h4 className="text-5xl text-white font-semibold font-Poppins">2,345</h4>
+            <Image
+              src="/images/payments.svg"
+              alt="USDC"
+              width={32}
+              height={32}
+            />
+            <h4 className="text-5xl text-white font-semibold font-Poppins">
+              {balance}
+            </h4>
             <h5 className="p-x-sm font-semibold text-white">USDC Earned</h5>
             <p className="p-xs text-white opacity-60">Since 13 Jan 2023</p>
           </div>
--- src/dashboard/portfolio/wallet.js
+++ src/dashboard/portfolio/wallet.js
@@ -1,7 +1,39 @@
-import Progress from "./progress";
-import TrasactionCard from "@/utils/TransactionCard";
+import NoTransaction from '../common/noTransaction';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import Hemergy from '@hemergy/core-sdk';
+import { ethers } from 'ethers';
+
+import Progress from './progress';
+import TrasactionCard from '@/utils/TransactionCard';
 
 const Wallet = () => {
+  const state = useSelector((state) => state);
+  const [balance, setBalance] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const e = await state.user.web3auth.connect();
+
+      const ethersProvider = new ethers.providers.Web3Provider(e);
+      const signer = await ethersProvider.getSigner();
+      console.log('signer address', await signer.getAddress());
+      const hemergy = new Hemergy({
+        baseURL: 'https://dev-core.hemergy.com',
+        signer,
+      });
+      const balance = await hemergy.getBalance(
+        state?.user.user?.accountAddress
+      );
+      console.log('balance', balance);
+
+      let hexNumber = balance._hex;
+      const bigIntNumber = BigInt(hexNumber);
+      const number = Number(bigIntNumber);
+      setBalance(number);
+    })();
+  }, [state?.user]);
+
   return (
     <div className="insight-card w-full laptop:w-[32%] bg-blue800 ">
       <div className="mb-8">
@@ -11,7 +43,7 @@ const Wallet = () => {
         </p>
       </div>
       <div className="mb-[60px]">
-        <Progress />
+        <Progress balance={balance} />
       </div>
       <div className="flex flex-col items-center justify-center gap-2 mb-8">
         <button className="secondary flex items-center justify-center rounded-xl text-white bg-blue700">
