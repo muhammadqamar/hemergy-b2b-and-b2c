7513387c35ed6bcc9f5f3e7971601b5662f4d422
muhammadqamar <muhammadqamar111@gmail.com>
2023-05-24 15:29:38 +0500

    fix errors

--- src/components/balance/index.js
+++ src/components/balance/index.js
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 const BalanceCom = () => {
     const {balance}  = useSelector(state=>state.user)
   return (
-    <div className=" bg-[#fff] p-[10px] rounded-[10px] fixed top-[8px] right-[8px] z-[10px] px-[20px] shadow-mainshadow flex items-center gap-[10px]">
+    <div className=" bg-[#fff] p-[10px] rounded-[10px] fixed top-[8px] right-[8px] z-[999] px-[20px] shadow-mainshadow flex items-center gap-[10px]">
       <img src="/images/token.png" alt="" />Available Balance : <strong>{balance || 0}  USDC</strong>
     </div>
   );
